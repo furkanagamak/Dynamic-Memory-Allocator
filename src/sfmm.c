56ceed6d972c0134c85f5061bf1fc642ee6ba931
furkanagamak <furkanagamak06@gmail.com>
2023-04-25 23:47:20 -0400

    Final Version

---
+++ src/sfmm.c
@@ -0,0 +1,25 @@
+/**
+ * Do not submit your assignment with a main function in this file.
+ * If you submit with a main function in this file, you will get a zero.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "debug.h"
+#include "sfmm.h"
+
+void *sf_malloc(size_t size) {
+    abort();
+}
+
+void sf_free(void *pp) {
+    abort();
+}
+
+void *sf_realloc(void *pp, size_t rsize) {
+    abort();
+}
+
+void *sf_memalign(size_t size, size_t align) {
+    abort();
+}
